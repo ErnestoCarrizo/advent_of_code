049fdfbcffcdacb131db1f10fab52f55dea14cf5
ErnestoCarrizo <67176861+ErnestoCarrizo@users.noreply.github.com>
2022-12-01 23:10:05 -0300

    updated

--- Day 1 Calorie Counting/solucion.py
+++ Day 1 Calorie Counting/solucion.py
@@ -2,13 +2,12 @@ file = open("input.txt", "r")
 input = file.readlines()
 maximo = 0
 sum = 0
-maximos = []
+
 for line in input:
     line = line.strip('\n')
     if line != '':
         sum += int(line)
     else:
         maximo = max(maximo, sum)
-        maximos.append(maximo)
         sum = 0
-print(sum(maximos.sort()[0..2]))
+print(maximo)
